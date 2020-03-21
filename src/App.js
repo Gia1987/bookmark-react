66626ab04de14f72193eae7be14c35454d3c0524
Giacomo87 <giacomo@data-lens.co.uk>
2020-03-21 23:53:37 +0000

    linkItem component, installed sass, implemented ability to remove and edit links.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React,{ useState} from 'react';
 import Header from './components/header';
 import Form from './components/form';
-import './styles/App.css';
+import LinkItem from './components/linkItem';
+import './styles/App.scss';
 
 function App() {
   const [links, setLinks] = useState([])
@@ -12,21 +13,34 @@ function App() {
     const newLinksArray = [...links, { id , link: linkText }];
     setLinks(newLinksArray)
   }
-  return (
-    <div className="App">
-      <div  className='link-wrapper'>
-        <Header />
-        <Form handleLink={addLink}/>
-        <ul>
-          {
-            links.map((link) => {
-              return link.link
-            })
-          }
-        </ul>
+  const removeLink = (id) => {
+    const newLinksArray = links.filter((link) => link.id !== id)
+    setLinks(newLinksArray)
+  }
+
+  const editLink = (value, id) => {
+    const linkToEdit = links.find((link) => link.id === id)
+    if(linkToEdit){
+      const newArray = links.slice()
+      newArray[id].link = value
+      setLinks(newArray)
+    }
+  }
+    return (
+      <div className="App">
+        <div  className='link-wrapper'>
+          <Header />
+          <Form handleLink={addLink}/>
+          <ul>
+            {
+              links.map((link) => {
+                return <LinkItem link={link} key={link.id} id={link.id} removeLink={removeLink} editLink={editLink}/>
+              })
+            }
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,10 +1,10 @@
 import React, {useState, Fragment} from 'react';
-import '../../styles/form.css';
+import '../../styles/form.scss';
 import PropTypes from 'prop-types'
 import { urlValidator } from '../../utils/urlValidator';
 
-export default function Form({handleLink}) {
-  const [websiteAddress, setAddress] = useState('');
+export default function Form({handleLink, link, onCloseModal}) {
+  const [websiteAddress, setAddress] = useState(link.link);
   const [errorMessage, setErrorValue] = useState(false);
 
   const handleChange =(e) => {
@@ -16,15 +16,16 @@ export default function Form({handleLink}) {
     e.preventDefault();
     // URL validation
     if (url.length > 0 && urlValidator(url)){
-      handleLink(url);
+      handleLink(url,link.id);
       setAddress('')
+      onCloseModal()
       return setErrorValue(false)
     }
       setErrorValue(true)
   }
     return(
       <Fragment>
-      <form className='form' onSubmit={(e) => updateInput(e, websiteAddress)}>
+      <form onSubmit={(e) => updateInput(e, websiteAddress)}>
         <input type='text'
          placeholder="Add website address"
          value={websiteAddress}
@@ -38,4 +39,15 @@ export default function Form({handleLink}) {
 
 Form.propTypes = {
   handleLink: PropTypes.func.isRequired,
+  onCloseModal: PropTypes.func,
+  link: PropTypes.shape({
+    id:PropTypes.node,
+    link: PropTypes.string
+   })
 }
+
+// Specifies the default values for props:
+Form.defaultProps = {
+  link: {id: null, link:''},
+  onCloseModal: () => {}
+};
